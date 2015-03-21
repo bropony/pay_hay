cdf588bb83ef6893b61eb47ed5e6b1dcf38b7417
bropony <ahda@qq.com>
2015-03-21 23:25:49 +0800

    js view

---
+++ Web/view/home.js
@@ -0,0 +1,13 @@
+/// <reference path="../rmi/rmidatastruct.ts" />
+/// <reference path="../rmi/rmiclient.ts" />
+/// <reference path="../rmi/rmimanager.ts" /> 
+var View;
+(function (View) {
+    var HomeScene = (function () {
+        function HomeScene() {
+        }
+        return HomeScene;
+    })();
+    View.HomeScene = HomeScene;
+})(View || (View = {}));
+//# sourceMappingURL=home.js.map
---
+++ Web/view/home.ts
@@ -0,0 +1,9 @@
+﻿/// <reference path="../rmi/rmidatastruct.ts" />
+/// <reference path="../rmi/rmiclient.ts" />
+/// <reference path="../rmi/rmimanager.ts" /> 
+
+module View {
+    export class HomeScene {
+        private postList: Rmi.SPost[];
+    }
+}
