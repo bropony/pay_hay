bf330ccd352a9138a05a9a2d545b0fa0ee4501a7
bropony <ahda@qq.com>
2015-04-02 11:56:42 +0800

    avatarImgId

--- Java/Serialize/src/rmi/client/SLoginReturn.java
+++ Java/Serialize/src/rmi/client/SLoginReturn.java
@@ -7,24 +7,28 @@ public class SLoginReturn{
 	public int userId;
 	public String nickname;
 	public byte[] avatar;
+	public int avatarImgId;
 	public String sessionKey;
 
 	public SLoginReturn(){
 		userId = 0;
 		nickname = "";
 		avatar = new byte[0];
+		avatarImgId = 0;
 		sessionKey = "";
 	}
 	public void read(SerializeStream __is){
 		userId = __is.read(userId);
 		nickname = __is.read(nickname);
 		avatar = __is.read(avatar);
+		avatarImgId = __is.read(avatarImgId);
 		sessionKey = __is.read(sessionKey);
 	}
 	public void write(SerializeStream __os){
 		__os.write(userId);
 		__os.write(nickname);
 		__os.write(avatar);
+		__os.write(avatarImgId);
 		__os.write(sessionKey);
 	}
 }
