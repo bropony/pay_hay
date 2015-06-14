3597757e2659167bb5022679fe19a7fbb158385d
bropony <ahda@qq.com>
2015-06-14 22:58:20 +0800

    database

--- Cpp/Server/WebServerApp/Db/DbHelper.cpp
+++ Cpp/Server/WebServerApp/Db/DbHelper.cpp
@@ -10,15 +10,15 @@ void CDbHelper::updateTUser(const Message::Db::Tables::TUser & t)
 
 void CDbHelper::updateTUserPost(const Message::Db::Tables::TUserPost & t)
 {
-	CDaoManager::instance()->getIUserPostDao();
+	CDaoManager::instance()->getIUserPostDao()->updateUserPost(t);
 }
 
 void CDbHelper::updateTUserComment(const Message::Db::Tables::TUserComment & t)
 {
-
+	CDaoManager::instance()->getIUserCommentDao()->updateUserComment(t);
 }
 
 void CDbHelper::updateTUserImg(const Message::Db::Tables::TUserImg & t)
 {
-
+	CDaoManager::instance()->getIUserImgDao()->updateUserImg(t);
 }
